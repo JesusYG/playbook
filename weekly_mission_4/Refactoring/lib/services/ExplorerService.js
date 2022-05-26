3f4ca6c03a5f082918cab5f548e21f4416e70904
JesusYG <gaal3283@gmail.com>
2022-05-26 23:43:30 +0100

    Agregando archivos Reader, ExplorerService, FizzbuzzService, sus respectivos archivos de pruebas asi como la dependencia jest e inicializacion de npm

---
+++ weekly_mission_4/Refactoring/lib/services/ExplorerService.js
@@ -0,0 +1,18 @@
+
+
+class ExplorerService{
+
+    static filterByMission(explorers, mission){
+        //
+    }
+
+    static getAmountOfExplorersByMission(explorers, mission){
+        //
+    }
+
+    static getExplorersUsernamesByMission(explorers, mission){
+        //
+    }
+}
+
+module.exports = ExplorerService
---
+++ weekly_mission_4/Refactoring/lib/services/FizzbuzzService.js
@@ -0,0 +1,5 @@
+class FizzbuzzService{
+
+}
+
+module.exports = FizzbuzzService
